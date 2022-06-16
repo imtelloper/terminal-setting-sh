141ea9de21f78b3855743bb4c02ced4fc7f729b3
imtelloper <h91oon@gmail.com>
2022-06-16 16:34:41 +0900

    update sh

---
+++ safety-backend-immortal.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt upgrade
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install python3.8 -y
+sudo apt install python-is-python3
+alias python=python3
+conda config --set auto_activate_base false
