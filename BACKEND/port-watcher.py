b1425fc04f5c4ad0f00a4f77e8f07f1cc6328573
imtelloper <h91oon@gmail.com>
2022-06-17 16:42:23 +0900

    port-wathcer updated as daemon

--- BACKEND/port-watcher.py
+++ BACKEND/port-watcher.py
@@ -2,24 +2,54 @@ import socket
 import subprocess
 import time
 import os
+import sys
+import daemon
+from daemon import pidfile
+from daemon import runner
+import argparse
 
-print("### Port watcher is running ###")
-portChecking = False
 
-while True:
-  time.sleep(5)
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  result = sock.connect_ex(('127.0.0.1', 8000)) # ip, port
+def runPortChecker():
+    print("### Port watcher is running ###")
+    while True:
+        time.sleep(5)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(('127.0.0.1', 8000))  # ip, port
 
-  # if portChecking == False :
-  if result == 0:
-    print('portChecking : ', portChecking)
-    print("Port is Open")
-    portChecking = False
-  else:
-    print("")
-    print('portChecking : ', portChecking)
-    print("Port is not Open")
-    os.system("pwd")
-    os.system("python /home/interx/SAFETY-AI/BACKEND/main.py")
-    portChecking = True
+        if result == 0:
+            print("Port is Open")
+        else:
+            print("")
+            print("Port is not Open")
+            os.system("pwd")
+            os.system("python /home/interx/SAFETY-AI/BACKEND/main.py")
+
+def runDaemon():
+    try:
+        pid = os.fork()
+
+        if pid > 0:
+            print
+            'PID: %d' % pid
+            sys.exit()
+
+    except OSError as error:
+        print
+        'Unable to fork. Error: %d (%s)' % (error.errno, error.strerror)
+        sys.exit()
+
+    doTask()
+
+
+def doTask():
+    "new session create"
+    os.setsid()
+
+    os.open("/dev/null", os.O_RDWR)
+    os.dup(0)
+    os.dup(0)
+
+    runPortChecker()
+
+if __name__ == '__main__':
+    runDaemon()
