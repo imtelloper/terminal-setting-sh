e2552640f9f3be8e303ead8992d5625e1dd876b3
이유빈 <dbqls2757h@naver.com>
2022-07-14 14:59:12 +0900

    add NavBar

--- APPLICATION/src/components/NavBar.tsx
+++ APPLICATION/src/components/NavBar.tsx
@@ -1,125 +1,133 @@
 import React from 'react';
 import '../style/components/NavBar.scss';
-import '../style/DesignSystem.scss';
+import 'style/DesignSystem.scss';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../images/safety.ai_logo.png';
-import PlusIcon from '../images/plusIcon.png';
-import { AppsSharp, FolderRounded, Person, Settings } from '@material-ui/icons';
+import {
+  Apps,
+  ArrowDropDown,
+  FolderRounded,
+  Person,
+  Settings,
+} from '@material-ui/icons';
+import { MdManageAccounts } from 'react-icons/md';
+import { BiLogOut } from 'react-icons/bi';
 
 const NaviBar = () => {
   const navigate = useNavigate();
 
-  const handleHamBtn = (e) => {
+  const handleMenuBar = (e) => {
     const target = e.currentTarget;
-    target.classList.toggle('hamBtnActive');
+    target.classList.toggle('menuBarActive');
   };
 
   return (
     <>
-      <div className="mainTopContainer">
-        <div>
-          <div className="hamBtnBox">
-            <div className="hamBtn" onClick={handleHamBtn}>
-              <span className="first" />
-              <span className="second" />
-              <span className="third" />
+      <div className="header">
+        <div className="menu">
+          <div className="view-all-menu">
+            <div className="menu-bar">
+              <span />
+              <span />
+              <span />
+            </div>
+            <div className="all-menu">
+              <ul>
+                <li>
+                  <p>Recipe.AI</p>
+                  <h5>Project NAME</h5>
+                  <ul>
+                    <li>
+                      <span><FolderRounded/></span>현상분석
+                    </li>
+                  </ul>
+                </li>
+              </ul>
             </div>
           </div>
-          <button
-            className="logo"
-            onClick={() => {
-              navigate('/');
-            }}
-          >
-            <img src={Logo} alt="" />
-          </button>
-          <div className="menuBar">
-            <ul>
-              <li>System Settings<span>▼</span></li>
-              <li>Manage Account<span>▼</span></li>
-              <button className="btnXS defaultPrimary">Company</button>
-            </ul>
-          </div>
-        </div>
-        <div className="iconBox">
-          <button
-            onClick={() => {
-              navigate('/login');
-            }}
-          >
-            <span className="box" />
-            <span className="icon">
-              <Person />
-            </span>
-          </button>
-          <button
-            onClick={() => {
-              navigate('/setting');
-            }}
-          >
-            <span className="box" />
-            <span className="icon">
-              <Settings />
-            </span>
-          </button>
-          <button
-            onClick={() => {
-              navigate('/setting');
-            }}
-          >
-            <span className="box" />
-            <span className="icon">
-              <AppsSharp/>
-            </span>
-          </button>
-        </div>
-      </div>
-      <div className="menuBox">
-        <div className="menuContent">
-          <div className="AIBox recipeAIBox">
-            <div className="titleBox">
-              <span>Recipe.AI</span>
-              <span>Project NAME</span>
-            </div>
-            <ul>
-              <li><span><FolderRounded/></span>현상분석</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-            </ul>
-          </div>
-          <div className="AIBox inspectionAIBox">
-            <div className="titleBox">
-              <span>Inspection.AI</span>
-              <span>Project NAME</span>
-            </div>
-            <ul>
-              <li><span><FolderRounded/></span>AI 비전 검사</li>
-              <li><span><FolderRounded/></span>Level 2 Menu
-                <ul className="subMenu">
-                  <li><span><FolderRounded/></span>Level 3 Menu</li>
-                  <li><span><FolderRounded/></span>Level 3 Menu</li>
-                  <li><span><FolderRounded/></span>Level 3 Menu</li>
-                </ul>
-              </li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-              <li><span><FolderRounded/></span>Level 2 Menu</li>
-            </ul>
-          </div>
-          <div className="AIBox inspectionAIBox"/>
-          <div className="AIBox inspectionAIBox">
-            <button className="iconBtnL defaultES">
-              <span className="iconL">
-                <img src={PlusIcon}/>
+          <div className="company-logo" />
+          <div className="block" />
+          <div className="breadcrumb">
+            <div className="btn-dropdown">
+              <span>
+                Project NAME / Level 1 Menu
+                <i><ArrowDropDown/></i>
               </span>
-              <span className="txt">BUTTON</span>
-            </button>
+              <ul className="dropdown-list">
+                <li>
+                  <span><FolderRounded /></span>menu1
+                </li>
+                <li>
+                  <span><FolderRounded /></span>menu2
+                </li>
+                <li>
+                  <span><FolderRounded /></span>menu3
+                </li>
+              </ul>
+            </div>
+            <div className="btn-dropdown">
+              <span>
+                Level 2 Menu <i><ArrowDropDown /></i>
+              </span>
+              <ul className="dropdown-list">
+                <li>
+                  <span><FolderRounded /></span>menu1
+                </li>
+                <li>
+                  <span><FolderRounded /></span>menu2
+                </li>
+                <li>
+                  <span><FolderRounded /></span>menu3
+                </li>
+              </ul>
+            </div>
+            <div className="btn-dropdown">
+              <span>
+                Level 3 Menu<i><ArrowDropDown /></i>
+              </span>
+              <ul className="dropdown-list">
+                <li>
+                  <span><FolderRounded /></span>menu1
+                </li>
+                <li>
+                  <span><FolderRounded /></span>menu2
+                </li>
+                <li>
+                  <span><FolderRounded /></span>menu3
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="block" />
+        </div>
+        <div className="private">
+          <div className="block" />
+          <div className="btn-dropdown">
+            <span><Person /></span>
+            <ul className="dropdown-list">
+              <li>
+                <span><MdManageAccounts /></span>My Page
+              </li>
+              <li>
+                <span><BiLogOut /></span>menu2
+              </li>
+            </ul>
+          </div>
+          <div className="btn-dropdown">
+            <span><Settings/></span>
+            <ul className="dropdown-list">
+              <li>menu1</li>
+              <li>menu2</li>
+              <li>menu3</li>
+            </ul>
+          </div>
+          <div className="group btn-dropdown">
+            <span><Apps /></span>
+            <ul className="dropdown-list">
+              <li>menu1</li>
+              <li>menu2</li>
+              <li>menu3</li>
+            </ul>
           </div>
         </div>
       </div>
