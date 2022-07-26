bed0ce7846478380023c31eb8580f0cbde598574
이유빈 <dbqls2757h@naver.com>
2022-07-26 13:09:54 +0900

    add page, fixed css

---
+++ APPLICATION/src/components/ImgPopup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/components/ImgPopup.scss';
+
+const ImgPopup = ({ closeImgPopup }) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ImgPopup;
