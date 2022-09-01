d11a88d33555a4f4bde1c10d3eddbd473b3dbe3b
imtelloper <h91oon@gmail.com>
2022-09-01 13:58:35 +0900

    ubuntu desktop setting version implementing...

--- safety-terminal-makeup.sh
+++ safety-terminal-makeup.sh
@@ -1,4 +1,3 @@
-#sudo sh /home/ubuntu/terminal-setting-sh/library/zsh-makeup.sh
 sudo sh /home/interx/terminal-setting-sh/library/hosting-tools-set.sh
 sudo sh /home/interx/terminal-setting-sh/library/brew-install.sh
 sudo sh /home/interx/terminal-setting-sh/library/nvm-install.sh
@@ -7,7 +6,5 @@ sudo sh /home/interx/terminal-setting-sh/library/python-install.sh
 sudo sh /home/interx/terminal-setting-sh/library/https-injection.sh
 sudo sh /home/interx/terminal-setting-sh/library/language-set.sh
 sudo sh /home/interx/terminal-setting-sh/library/anaconda-install.sh
-sudo sh /home/interx/terminal-setting-sh/library/mysql-install.sh
 sudo sh /home/interx/terminal-setting-sh/library/mongodb-install.sh
 sudo systemctl status mongod
-sudo systemctl status mysql
