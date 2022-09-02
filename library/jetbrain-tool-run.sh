426c305b67ac37e31094348ea248f9f765f6142d
imtelloper <h91oon@gmail.com>
2022-09-02 09:46:39 +0900

    ubuntu desktop setting version implementing...

--- library/jetbrain-tool-run.sh
+++ library/jetbrain-tool-run.sh
@@ -1,7 +1,7 @@
 ######################################
 # jetbrain tool box를 먼저 다운 받으세요!!!
 ######################################
-
+sudo apt install libfuse2
 cd /opt
 sudo tar -xvzf ~/Downloads/jetbrains-toolbox-1.25.12627.tar.gz
 sudo mv jetbrains-toolbox-1.25.12627 jetbrains
